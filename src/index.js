7117c19c582969137dbfaf06165b58b848ff2faa
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-04-23 19:35:03 +0545

    Add basic form for password generation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import generatePassword from 'password-generator';
 
-console.log(generatePassword());
+const optionRange = document.getElementById('option-length-range');
+const optionLength = document.getElementById('option-length');
+
+optionRange.addEventListener('change', (e) => {
+  optionLength.value = e.target.value;
+});
+
+optionLength.addEventListener('change', (e) => {
+  optionRange.value = e.target.value;
+});
+
+const form = document.getElementById('password-form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const numbers = formData.get('option-numbers') === 'on';
+  const lowerCase = formData.get('option-lowercase') === 'on';
+  const upperCase = formData.get('option-uppercase') === 'on';
+  const symbols = formData.get('option-symbols') === 'on';
+  const length = Number(formData.get('option-length'));
+
+  const options = {
+    numbers,
+    lowerCase,
+    upperCase,
+    symbols,
+    length,
+  };
+
+  console.log(generatePassword(options));
+});
