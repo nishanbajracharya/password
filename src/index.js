1ac19563c2ff68dafcd8bcbb5c1c2b007b18dd90
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-04-23 22:13:06 +0545

    Add styles for form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import generatePassword from 'password-generator';
 
+const RANGE_DEFAULT = 12;
+const RANGE_MIN = 2;
+const RANGE_MAX = 32;
+
 const optionRange = document.getElementById('option-length-range');
 const optionLength = document.getElementById('option-length');
 
-optionRange.addEventListener('change', (e) => {
+function setInputRange(input) {
+  input.setAttribute('min', RANGE_MIN);
+  input.setAttribute('max', RANGE_MAX);
+  input.setAttribute('value', RANGE_DEFAULT);
+}
+
+setInputRange(optionRange);
+setInputRange(optionLength);
+
+optionRange.addEventListener('input', (e) => {
   optionLength.value = e.target.value;
 });
 
-optionLength.addEventListener('change', (e) => {
-  optionRange.value = e.target.value;
+optionLength.addEventListener('input', (e) => {
+  let value = Number(e.target.value);
+
+  if (value > RANGE_MAX) value = RANGE_MAX;
+  if (value < RANGE_MIN) value = RANGE_MIN;
+
+  optionRange.value = String(value);
+
+  setTimeout(() => {
+    optionLength.value = String(value);
+  }, 250);
 });
 
 const form = document.getElementById('password-form');
+
+const result = document.getElementById('result');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -32,5 +56,5 @@ form.addEventListener('submit', function (e) {
     length,
   };
 
-  console.log(generatePassword(options));
+  result.textContent = generatePassword(options);
 });
