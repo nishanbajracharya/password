3ac5ba7db65683d574fd533ae8adc968b929cdbe
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-04-30 14:34:27 +0545

    Updates

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import iziToast from '../_snowpack/pkg/izitoast.js';
+import '../_snowpack/pkg/izitoast/dist/css/iziToast.min.css.proxy.js';
+import generatePassword from '../_snowpack/pkg/password-generator.js';
+
+const resultContainer = document.getElementById('result-container');
+const copyButton = document.getElementById('copy-button');
+
+const RANGE_DEFAULT = 12;
+const RANGE_MIN = 2;
+const RANGE_MAX = 32;
+
+const optionRange = document.getElementById('option-length-range');
+const optionLength = document.getElementById('option-length');
+
+let password = '';
+
+function setInputRange(input) {
+  input.setAttribute('min', RANGE_MIN);
+  input.setAttribute('max', RANGE_MAX);
+  input.setAttribute('value', RANGE_DEFAULT);
+}
+
+setInputRange(optionRange);
+setInputRange(optionLength);
+
+optionRange.addEventListener('input', (e) => {
+  optionLength.value = e.target.value;
+});
+
+optionLength.addEventListener('input', (e) => {
+  let value = Number(e.target.value);
+
+  if (value > RANGE_MAX) value = RANGE_MAX;
+  if (value < RANGE_MIN) value = RANGE_MIN;
+
+  optionRange.value = String(value);
+
+  setTimeout(() => {
+    optionLength.value = String(value);
+  }, 250);
+});
+
+const form = document.getElementById('password-form');
+
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const numbers = formData.get('option-numbers') === 'on';
+  const lowerCase = formData.get('option-lowercase') === 'on';
+  const upperCase = formData.get('option-uppercase') === 'on';
+  const symbols = formData.get('option-symbols') === 'on';
+  const length = Number(formData.get('option-length'));
+
+  const options = {
+    numbers,
+    lowerCase,
+    upperCase,
+    symbols,
+    length,
+  };
+
+  password = generatePassword(options);
+  result.textContent = password;
+  resultContainer.classList.add('show');
+});
+
+copyButton.addEventListener('click', function () {
+  password && copyText(password);
+});
+
+function copyText(text = '') {
+  const input = document.createElement('input');
+  document.body.appendChild(input);
+
+  input.value = text;
+
+  input.select();
+
+  document.execCommand('copy');
+
+  document.body.removeChild(input);
+
+  iziToast.show({
+    theme: 'dark',
+    displayMode: 1,
+    title: 'Copied!',
+    progressBar: false,
+    position: 'bottomCenter',
+  });
+}
